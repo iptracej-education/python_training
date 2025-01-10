91754d62ac81c4ea256d550edf1d4ff996175c45
iptracej-education <iptracej@hotmail.com>
2025-01-09 23:33:43 -0500

    make it shorten

--- 6kyu/valid_braces_v2.py
+++ 6kyu/valid_braces_v2.py
@@ -1,11 +1,14 @@
 def valid_braces(string):
+    
     stack = []
     pair ={']': '[','}': '{', ')': '(',}
+    
     for char in string:
         if char in '[{(':
             print(char)
             stack.append(char)
         if char in ']})':
-            if len(stack) == 0 or pair[char] != tstack.pop():
+            if len(stack) == 0 or pair[char] != stack.pop():
                 return False
+    
     return not stack
